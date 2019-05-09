34077d59964a6c58f367613691bacb4d93009e08
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-09 20:58:44 +0200

    final collapsible component

--- src/components/Collapsibles.js
+++ src/components/Collapsibles.js
@@ -1,21 +1,26 @@
 import React from 'react';
 
-class Collapsibles extends React.Component { 
-    render(){
-        return(
-    <legend className="legend closed"> <i className="far fa-object-ungroup fa-lg legend__icon"></i>
-        <h2 className="legend__title">Diseña</h2>
-        <i className="fas fa-chevron-up legend__arrow"></i>
-    </legend>
-        );
-    }
+class Collapsibles extends React.Component {
+  render() {
+    return (
+      <legend className="legend closed">
+        {' '}
+        <i className={this.props.icon} />
+        <h2 className="legend__title">{this.props.title}</h2>
+        <i className="fas fas fa-chevron-up legend__arrow" />
+      </legend>
+    );
+  }
 }
 
 export default Collapsibles;
 
-
 // PLAN ****
- {/* cambiar class del primer icono  */} 
-        
-    {/* cambiar inner de h2 */}
-    // El contenido será un prop
+{
+  /* cambiar class del primer icono  */
+}
+
+{
+  /* cambiar inner de h2 */
+}
+// El contenido será un prop
