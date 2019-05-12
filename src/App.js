f4c438b620a8155d2a174c82024802819c28e6f7
Carmen Pilar Moreno Ramos <48130270+carmenpmoreno@users.noreply.github.com>
2019-05-12 19:45:03 +0200

    change React import

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './scss/main.scss';
 import Collapsibles from './components/Collapsibles';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
