91b246ce40fb8b677b95d040053f957d79ec7b86
carmenpmoreno <carmenp.moreno@gmail.com>
2019-05-20 20:12:14 +0200

    remove comented lines

--- src/components/FillInItem.js
+++ src/components/FillInItem.js
@@ -1,33 +1,6 @@
 import React from 'react';
 
 class FillInItem extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     // data: { **NOTA, SI COMENTO "data" si que puedo guardar en el objeto el cambio de estado del valor "name", que pasa de vacío a lleno con lo escrito en el input
-  //       palette: 1,
-  //       name: '',
-  //       job: '',
-  //       phone: '',
-  //       email: '',
-  //       linkedin: '',
-  //       github: '',
-  //       photo: '',
-  //     // },
-  //     collapsible: false
-  //   };
-  //   this.handleInputChange = this.handleInputChange.bind(this);
-  //  // console.log(this.state.data.name);
-  // }
-  // handleInputChange(event) {
-  //   const key = event.target.name;
-  //   this.setState({
-  //     [key]: event.target.value
-  //   });
-  //   console.log(key)
-  //   console.log(event.target.value)
-  // }
-
   render() {
     return (
       <div className='fill-in__item'>
